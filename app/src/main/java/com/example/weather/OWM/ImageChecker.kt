007c9b749dc8e83965b4f5148c48c3f139fb27c6
AndrKriv <krivandr01@yandex.by>
2021-12-09 22:34:52 +0300

    Изображения
    Проверка состояния погоды и подстановка нужного изображения в recyclerView

---
+++ app/src/main/java/com/example/weather/OWM/ImageChecker.kt
@@ -0,0 +1,24 @@
+package com.example.weather.OWM
+
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.weather.R
+
+object ImageChecker{
+    fun image_weather(string_checker:String):Int{
+        return when(string_checker) {
+            "ясно" -> R.drawable.sun
+            "пасмурно" -> R.drawable.cloudy
+            "облачно с прояснениями" -> R.drawable.cloud_sun
+            "небольшая облачность" -> R.drawable.cloud
+            "переменная облачность" -> R.drawable.cloud
+            "сильный дождь" -> R.drawable.rain
+            "дождь" -> R.drawable.rain
+            "небольшой дождь" -> R.drawable.rain_small
+            "проливной дождь" -> R.drawable.rain
+            else -> R.drawable.unknown
+
+        }
+    }
+}
