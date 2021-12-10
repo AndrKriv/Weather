718192a8c7f86dab52a57576de86d8642c8c2b17
AndrKriv <krivandr01@yandex.by>
2021-12-10 18:09:42 +0300

    Unimportant changes

--- app/src/main/java/com/example/weather/BottomActivity.kt
+++ app/src/main/java/com/example/weather/BottomActivity.kt
@@ -1,26 +1,33 @@
 package com.example.weather
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.weather.connection.CheckConnection
 import com.example.weather.databinding.ActivityBottomBinding
 
 class BottomActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBottomBinding
-
+    val connection = CheckConnection()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityBottomBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
 
-        val navView: BottomNavigationView = binding.navView
-        navView.setupWithNavController(navController)
+        if(connection.checkForInternet(this)) {
+            val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
+
+            val navView: BottomNavigationView = binding.navView
+            navView.setupWithNavController(navController)
+        }
+        else Toast.makeText(this,"Проверьте Ваше интернет-соединение!",Toast.LENGTH_SHORT).show()
+
     }
 }
